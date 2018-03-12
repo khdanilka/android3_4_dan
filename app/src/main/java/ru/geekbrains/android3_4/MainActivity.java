25e75c5c150f4dd52e7440624ac0c8da19d2dd4b
stanislav <stanislav.khizhnyak@gmail.com>
2018-03-12 17:38:05 +0300

    okhttp

--- app/src/main/java/ru/geekbrains/android3_4/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android3_4/MainActivity.java
@@ -2,14 +2,58 @@ package ru.geekbrains.android3_4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    private void getByOkhttp()
+    {
+
+        Single.fromCallable(() ->
+        {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://api.github.com/users/skhizhnyak")
+                    .build();
+            try
+            {
+                return client.newCall(request).execute().body().string();
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(s ->Log.d(TAG, s), t -> Log.e(TAG, "error", t));
+
+
     }
 }
